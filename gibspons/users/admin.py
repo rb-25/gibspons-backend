049f8e15d036dd7cfcb402d5f3e44a764dc7fc67
rb-25 <aarabibalakrishnan2@gmail.com>
2024-04-25 12:49:22 +0530

    feat: added to admin site

--- gibspons/users/admin.py
+++ gibspons/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Organisation
+from .models import User, Organisation,OTP
 
 admin.site.register(User)
 admin.site.register(Organisation)
+admin.site.register(OTP)
